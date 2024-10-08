4a9eb39e2422f9181180783a437549f74f16ac70
Abhishek Satpute <abhisheksatpute2005@gmail.com>
2024-10-08 16:51:15 +0530

    Update task.java

--- task.java
+++ task.java
@@ -7,113 +7,111 @@
 // Features: Create accounts, deposit money, withdraw money, view balance.
 
 // Swing Elements: Use JTextField for account details, JButton for deposit/withdraw, JLabel for showing current balance.
-import javax.swing.*;
-import java.awt.*;
+// Classes: Account, Customer
+
+// Features: Create accounts, deposit money, withdraw money, view balance.
+
+// Swing Elements: Use JTextField for account details, JButton for deposit/withdraw, JLabel for showing current balance.
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
- class Account {
-    String accountNumber;
-    String accountHolder;
-    double balance;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTree.DynamicUtilTreeNode;
 
-     Account(String accountNumber, String accountHolder) {
-        this.accountNumber = accountNumber;
-        this.accountHolder = accountHolder;
-        this.balance = 0;
-    }
+class bancksyste {
+    static double balance = 0;
 
-    void deposit(double amount) {
-        balance += amount;
-    }
-
-    void withdraw(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            System.out.println("" + balance);
-        }
-        System.out.println("" + balance);
-    }
-}
-
-class task   {
- 
     public static void main(String[] args) {
-        double balance = 0.0;
 
-        JFrame frame = new JFrame(" Simple Banking System");
-        frame.setBounds(100, 120, 650, 750);
+        JFrame frame = new JFrame(" New task");
+        frame.setSize(550, 300);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel label = new JLabel("Enter Ac/holder Name:");
-        label.setBounds(40, 50, 150, 30);
-        frame.add(label);
 
-        JTextField textField = new JTextField();
-        textField.setBounds(170, 50, 190, 30);
+        // label
+        JLabel l1 = new JLabel("AccountHolder Name :");
+        l1.setBounds(20, 20, 160, 30);
+        frame.add(l1);
+        JTextField textField = new JTextField(10);
+        textField.setBounds(150, 20, 160, 30);
         frame.add(textField);
-
-        JLabel label1 = new JLabel("Enter AcountNumber:");
-        label1.setBounds(40, 90, 160, 30);
+        // account number :
+        JLabel l2 = new JLabel("Account Number :");
+        l2.setBounds(20, 60, 160, 30);
+        frame.add(l2);
+        JTextField textField2 = new JTextField(10);
+        textField2.setBounds(130, 60, 160, 30);
+        frame.add(textField2);
+        JButton button = new JButton("Create Ac:");
+        button.setBounds(300, 60, 120, 30);
+        frame.add(button);
+        JLabel label1 = new JLabel("");
+        label1.setBounds(60, 220, 350, 30);
         frame.add(label1);
 
-        JTextField textField1 = new JTextField();
-        textField1.setBounds(170, 90, 190, 30);
-        frame.add(textField1);
-
-        JLabel label2 = new JLabel("Enter Amount :");
-        label2.setBounds(40, 140, 160, 30);
-        frame.add(label2);
-
-        JTextField textField2 = new JTextField();
-        textField2.setBounds(170, 140, 190, 30);
-        frame.add(textField2);
-
-        JButton button = new JButton("Submit ");
-        button.setBounds(60, 190, 120, 30);
-        frame.add(button);
-        JButton button1 = new JButton("Deposite ");
-        button1.setBounds(180, 190, 120, 30);
+        // withdeow
+        JLabel l3 = new JLabel(" Transction Amount :");
+        l3.setBounds(20, 100, 160, 30);
+        frame.add(l3);
+        JTextField textField3 = new JTextField(10);
+        textField3.setBounds(130, 100, 160, 30);
+        frame.add(textField3);
+        JButton button1 = new JButton(" Deposite:");
+        button1.setBounds(300, 100, 120, 30);
         frame.add(button1);
-        JButton button2 = new JButton("Withadrow ");
-        button2.setBounds(300, 190, 120, 30);
-        frame.add(button2);
-        JButton button3 = new JButton("Show current Balance  ");
-        button3.setBounds(420, 190, 120, 30);
-        frame.add(button3);
-        JTextArea display = new JTextArea();
 
-        display.setEditable(false);
+        JButton button4 = new JButton("Withdraw:");
+        button4.setBounds(130, 140, 120, 30);
+        frame.add(button4);
+        JLabel label4 = new JLabel("");
+        label4.setBounds(300, 140, 150, 40);
+        frame.add(label4);
 
-        JScrollPane scrollPane = new JScrollPane(display);
+        // JTextArea display = new JTextArea();
+        // display.setEditable(false);
+        // JScrollPane scrollPane = new JScrollPane(display);
+        // scrollPane.setBounds(80, 150, 260, 60);
+        // frame.add(scrollPane);
 
-        scrollPane.setBounds(40, 250, 500, 200);
-
-        frame.add(scrollPane);
         button.addActionListener(new ActionListener() {
-
             public void actionPerformed(ActionEvent e) {
-
-                String name = textField.getText();
-
-                String name1 = textField1.getText();
+                String name1 = textField.getText();
                 String name2 = textField2.getText();
-
-                display.append("Ac/holder Name: " + name + "\n");
-
-                display.append(" Account no: " + name1 + "\n");
-                display.append("Amount: " + name2 + "\n\n");
+                label1.setText("Ac/holder Name  :" + name1 + "Ac Number :" + name2);
 
             }
         });
-        JPanel balancePanel = new JPanel();
-        JLabel label3 = new JLabel("Current Balance: $0");
-        label3.setBounds(40, 600, 120, 60);
-        double balance1 = 00;
-        frame.add(label3);
-
+        // butten deposite
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double amount = Double.parseDouble(textField3.getText());
+                balance += amount;
+                label4.setText("Balance $:" + balance);      
+                textField3.setText("");
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                double amount = Double.parseDouble(textField3.getText());
+                if (amount > balance) {
+                    JOptionPane.showMessageDialog(frame, "Insufficient funds");
+                } else {
+                    balance -= amount;
+                    label4.setText("Balance: $" + balance);
+                    textField3.setText("");
+                }
+            }
+        });
         frame.setVisible(true);
-Account obj = new Account("2345456", "Abhi ");
-    }
 
+    }
 }
+
+
